f7435cc5143ec462d8d8af9224937c2629160c15
OlenaPobehailo <olena.pobehailo@gmail.com>
2022-11-30 02:46:37 +0200

    add gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
+/*
+Додай бібліотеку SimpleLightbox як залежність проекту, використовуючи npm (посилання на CDN з твоєї минулої роботи більше не потрібне).
+Використовуй свій JavaScript код з попередньої домашньої роботи, але виконай рефакторинг з урахуванням того, що бібліотека була встановлена через npm (синтаксис import/export).
+Для того щоб підключити CSS код бібліотеки в проект, необхідно додати ще один імпорт, крім того, що описаний в документації.
+*/
+
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+galleryEl.addEventListener('click', selectImage);
+
+function makeGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join('');
+}
+
+function selectImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captions: true,
+  captionsData: 'alt',
+  captionSelector: 'img',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+//console.log(lightbox);
